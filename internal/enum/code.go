1252121a6081ad6c44394182f8bf4175bdd2fd23
hardyzp <31343168+hardyzp@users.noreply.github.com>
2025-11-20 14:36:03 +0800

    add error code

--- internal/enum/code.go
+++ internal/enum/code.go
@@ -13,4 +13,10 @@ const (
 	SuccessCode         = 0      // 成功
 	InvalidParamsCode   = -1     // 无效参数（自定义）
 	InvalidResponseCode = -2     // 无效响应（自定义）
+	NotSetGroupType     = 340001
+	NotSetGroupName     = 340002
+	GroupNameTooLong    = 340003
+	InvalidGroupType    = 340004
+	GroupIntroductionTooLong = 340005
+	GroupNotificationTooLong = 340006
 )
